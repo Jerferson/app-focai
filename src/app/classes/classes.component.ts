1704761f52d2645b30fbdadf4be40882d72899e4
Jerferson <jerfersongriza@gmail.com>
2022-11-27 11:31:36 -0300

    refactor: change input type

--- src/app/classes/classes.component.ts
+++ src/app/classes/classes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-classes',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./classes.component.css']
 })
 export class ClassesComponent {
+  showPorcentage: boolean = false
+  showSugestions: boolean = false
+  classesForm!: FormGroup
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
 
+
+  ngOnInit(): void {
+    this.classesForm = this.formBuilder.group({
+      class: new FormControl('', [Validators.required]),
+      subject: new FormControl('', [Validators.required]),
+      content: new FormControl('', [Validators.required])
+    })
+  }
+
+  filterClassChange() {
+    if (this.classesForm.controls['class'].getRawValue() !== 'Selecione a turma') {
+      this.showPorcentage = true
+    } else {
+      this.showPorcentage = false
+    }
+  }
+
+  filterSubjectChange() {
+
+  }
+
+  filterContentChange() {
+    if (this.classesForm.controls['content'].getRawValue() !== 'Alfabeto' && this.classesForm.controls['content'].getRawValue() !== 'Sílabas') {
+      this.showSugestions = false
+    } else {
+      this.showSugestions = true
+    }
+  }
 }
